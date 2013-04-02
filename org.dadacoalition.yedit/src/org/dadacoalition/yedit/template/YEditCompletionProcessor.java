67ca24e04ea29c88df3e580b6b468662d1edf0a7
shubham-m <shubham.meh@gmail.com>
2013-04-03 00:39:01 +0530

    Modified to allowed merging of suggestions

--- org.dadacoalition.yedit/src/org/dadacoalition/yedit/template/YEditCompletionProcessor.java
+++ org.dadacoalition.yedit/src/org/dadacoalition/yedit/template/YEditCompletionProcessor.java
@@ -1,43 +1,64 @@
 package org.dadacoalition.yedit.template;
 
+import java.util.List;
+
 import org.dadacoalition.yedit.Activator;
 import org.dadacoalition.yedit.YEditLog;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.jface.text.templates.Template;
 import org.eclipse.jface.text.templates.TemplateCompletionProcessor;
 import org.eclipse.jface.text.templates.TemplateContextType;
 import org.eclipse.swt.graphics.Image;
 
-/**
- * Completion processor used for YEdit templates. 
- */
-public class YEditCompletionProcessor extends TemplateCompletionProcessor {
+import shubhamm.yedit.contentassist.YAMLSchemaCompletionProcessor;
 
-    protected TemplateContextType getContextType(ITextViewer viewer, IRegion region) {
-        YEditLog.logger.info( "called getContextType" );
-        return Activator.getDefault().getContextTypeRegistry().getContextType(YAMLContentType.YAML_CONTENT_TYPE);
+public class YEditCompletionProcessor extends TemplateCompletionProcessor
+{
+  private YAMLSchemaCompletionProcessor schemaProposer;
+
+  public YEditCompletionProcessor()
+  {
+    this.schemaProposer = new YAMLSchemaCompletionProcessor();
+  }
+
+  protected TemplateContextType getContextType(ITextViewer viewer, IRegion region)
+  {
+    YEditLog.logger.info("called getContextType");
+    return Activator.getDefault().getContextTypeRegistry().getContextType("org.dadacoalition.yedit.template.yaml");
+  }
+
+  protected Image getImage(Template template) {
+    return null;
+  }
+
+  protected Template[] getTemplates(String contextTypeId)
+  {
+    YEditLog.logger.info("called getTemplates");
+    Template[] templates = Activator.getDefault().getTemplateStore().getTemplates();
+    YEditTemplate[] yeditTemplates = new YEditTemplate[templates.length];
+
+    for (int i = 0; i < templates.length; i++) {
+      yeditTemplates[i] = new YEditTemplate(templates[i]);
     }
+    return yeditTemplates;
+  }
 
-    protected Image getImage(Template template) {
-        return null;
+  public ICompletionProposal[] computeCompletionProposals(ITextViewer viewer, int offset)
+  {
+    List<ICompletionProposal> schemaProposals = this.schemaProposer.computeCompletionProposalsFromSchema(viewer, offset);
+    ICompletionProposal[] templateProposals = super.computeCompletionProposals(viewer, offset);
+    ICompletionProposal[] proposals = new ICompletionProposal[schemaProposals.size() + templateProposals.length];
+    int index = 0;
+    for (ICompletionProposal proposal : schemaProposals)
+    {
+      proposals[(index++)] = proposal;
     }
-
-    /**
-     * @return All the templates for the specified context type id. All the template objects
-     * are actually YEditTemplate objects and not Template objects. By returning YEditTemplate
-     * objects we can override the default match() method in template and get more sensible
-     * template matching.
-     */
-    protected Template[] getTemplates(String contextTypeId) {
-        YEditLog.logger.info( "called getTemplates" );
-        Template[] templates = Activator.getDefault().getTemplateStore().getTemplates();
-        YEditTemplate[] yeditTemplates = new YEditTemplate[templates.length]; 
-        
-        for( int i = 0; i < templates.length; i++ ){
-            yeditTemplates[i] = new YEditTemplate( templates[i] ); 
-        }
-        return yeditTemplates;
-    }       
-
+    for (ICompletionProposal proposal : templateProposals)
+    {
+      proposals[(index++)] = proposal;
+    }
+    return proposals;
+  }
 }
